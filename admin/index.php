0c7cd2d2e098ff73a09caa551e89d7d82f830963
KHANIT2014 <khan2014it@gmail.com>
2024-10-03 22:21:34 +0530

    add index functions and update index main page admin

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <?php
 include "./database/db.php";
+include "index_function.php";
 session_start();
 ob_start();
 ?>
@@ -253,6 +254,122 @@ ob_start();
                     </div>
                 </div>
                 <!-- /.row -->
+                 <!-- /.row -->
+                
+<div class="row">
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-file-text fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <?php  
+                        view_Posts();
+                        ?>
+
+
+                  <!-- <div class='huge'>12</div> -->
+                        <div>Posts</div>
+                    </div>
+                </div>
+            </div>
+            <a href="posts.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-comments fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                     <div class='huge'>23</div>
+                      <div>Comments</div>
+                    </div>
+                </div>
+            </div>
+            <a href="comments.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-yellow">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-user fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                    <div class='huge'>23</div>
+                        <div> Users</div>
+                    </div>
+                </div>
+            </div>
+            <a href="users.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-red">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-list fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class='huge'>13</div>
+                         <div>Categories</div>
+                    </div>
+                </div>
+            </div>
+            <a href="categories.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+                <!-- /.row -->
+
+
+            <!-- // 8114390434  20666  HP Services Center Odisha
+            06743568739 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             </div>
             <!-- /.container-fluid -->
---
+++ admin/index_function.php
@@ -0,0 +1,61 @@
+<?php
+include "./database/db.php";
+session_start();
+ob_start();
+
+function view_Posts(){
+    global $connection;
+
+    $sql_posts= "SELECT * FROM posts";
+    $sql_posts_query = mysqli_query($connection,$sql_posts);
+    $post_count = mysqli_num_rows($sql_posts_query);
+    echo "<div class='huge'>$post_count</div>";
+}
+
+function view_comments(){
+    global $connection;
+
+    $sql_comments= "SELECT * FROM comments";
+    $sql_comments_query = mysqli_query($connection,$sql_comments);
+    $comment_count = mysqli_num_rows($sql_comments_query);
+    echo "<div class='huge'>$comment_count</div>";
+}
+
+
+function view_Users(){
+    global $connection;
+
+    $sql_users= "SELECT * FROM users";
+    $sql_users_query = mysqli_query($connection,$sql_users);
+    $user_count = mysqli_num_rows($sql_users_query);
+    echo "<div class='huge'>$user_count</div>";
+}
+
+function view_Categories(){
+    global $connection;
+
+    $sql_categories= "SELECT * FROM categories";
+    $sql_categories_query = mysqli_query($connection,$sql_categories);
+    $category_count = mysqli_num_rows($sql_categories_query);
+    echo "<div class='huge'>$category_count</div>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
