aa21c8eecf5d3bee8af3d7f9995aecd430aedf93
KHANIT2014 <khan2014it@gmail.com>
2024-10-05 17:35:12 +0530

    updated Dashboard Admin Section

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,9 @@ include "index_function.php";
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
+
+    <!-- Google charts  -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -170,7 +172,7 @@ include "index_function.php";
 
 
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-             
+
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li>
@@ -199,7 +201,7 @@ include "index_function.php";
                             </li>
                         </ul>
                     </li>
-                 
+
                     <li>
                         <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
                     </li>
@@ -233,7 +235,7 @@ include "index_function.php";
 
 
 
-        
+
 
         <div id="page-wrapper">
 
@@ -248,7 +250,7 @@ include "index_function.php";
                         </h1>
                         <ol class="breadcrumb">
                             <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                                <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
                             </li>
                             <li class="active">
                                 <i class="fa fa-file"></i> Blank Page
@@ -257,155 +259,262 @@ include "index_function.php";
                     </div>
                 </div>
                 <!-- /.row -->
-                 <!-- /.row -->
-                
-<div class="row">
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-file-text fa-5x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <?php  
-                        view_Posts();
-                        ?>
-
-
-                  <!-- <div class='huge'>12</div> -->
-                        <div>Posts</div>
-                    </div>
-                </div>
-            </div>
-            <a href="posts.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-green">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-comments fa-5x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                    <?php  
-                        view_comments();
-                        ?>
-                      <div>Comments</div>
-                    </div>
-                </div>
-            </div>
-            <a href="comments.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-yellow">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-user fa-5x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                    <?php  
-                        view_Users();
-                        ?>
-                        <div> Users</div>
-                    </div>
-                </div>
-            </div>
-            <a href="users.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-red">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-list fa-5x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                    <?php  
-                        view_Categories();
-                        ?>
-                         <div>Categories</div>
-                    </div>
-                </div>
-            </div>
-            <a href="categories.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <!-- chart details  -->
-</div>
                 <!-- /.row -->
 
-
-
-
-
-
-
                 <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php
+                                        view_Posts();
+                                        ?>
+
+
+                                        <!-- <div class='huge'>12</div> -->
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="posts.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php
+                                        view_comments();
+                                        ?>
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php
+                                        view_Users();
+                                        ?>
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php
+                                        view_Categories();
+                                        ?>
+                                        <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="categories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- chart details  -->
+                </div>
+                <!-- /.row -->
+                <?php
+
+                $sql_posts = "SELECT * FROM posts";
+                $sql_posts_query = mysqli_query($connection, $sql_posts);
+                $post_count = mysqli_num_rows($sql_posts_query);
+                // echo "<div class='huge'>$post_count</div>";
+
+
+
+
+
+                $sql_users = "SELECT * FROM users";
+                $sql_users_query = mysqli_query($connection, $sql_users);
+                $user_count = mysqli_num_rows($sql_users_query);
+                // echo "<div class='huge'>$user_count</div>";
+
+
+
+
+
+                $sql_categories = "SELECT * FROM categories";
+                $sql_categories_query = mysqli_query($connection, $sql_categories);
+                $category_count = mysqli_num_rows($sql_categories_query);
+                // echo "<div class='huge'>$category_count</div>";
+
+
+
+
+
+                $sql_comments = "SELECT * FROM comments";
+                $sql_comments_query = mysqli_query($connection, $sql_comments);
+                $comment_count = mysqli_num_rows($sql_comments_query);
+                // echo "<div class='huge'>$comment_count</div>";
+
+
+
+                
+
+
+
+
+                ?>
                 <script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
+                    google.charts.load('current', {
+                        'packages': ['bar']
+                    });
+                    google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Data', 'Count'],
-          <?php
-        //   include 'index_function.php';
-        //   $Elements = ['Posts','users','comments','categories'];
-        //   $Elements_values= ['.$post_count.']
-        chart_detail();
-          
-          ?>
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                            ['Data', 'Count'],
+                            // ['Posts', '1000'],
+                           <?php
+                           $elements =['Active Posts','Active Users','Active Comments','Active Categories'];
 
+                           $ele_values= [$post_count,$user_count,$comment_count,$category_count];
 
-          ['2014', 1000]
-         
-        ]);
+                           for($i=0;$i<4; $i++){
 
-        var options = {
-          chart: {
-            title: 'Admin Record',
-            subtitle: '',
-          }
-        };
+                            echo "['{$elements[$i]}'" . "," . "{$ele_values[$i]} ],"  ;
+                            
+                           }
+                           ?>
 
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+                            
+                        ]);
 
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
-    </script>
+                       
 
 
-<div id="columnchart_material" style="width: 800px; height: 500px;"></div>
-   </div>
+                        var options = {
+                            chart: {
+                                title: 'Company Performance',
+                                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                            }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                    }
+                </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <!-- Existing code for chart some how not working  -->
+
+                <!-- <div class="row">
+                    <script type="text/javascript">
+                        google.charts.load('current', {
+                            'packages': ['bar']
+                        });
+                        google.charts.setOnLoadCallback(drawChart);
+
+                        function drawChart() {
+                            var data = google.visualization.arrayToDataTable([
+                                ['Data', 'Count'],
+                                <?php
+                                //   include 'index_function.php';
+                                $Elements = ['Posts', 'users', 'comments', 'categories'];
+                                $Elements_values = [$post_count, $user_count, $comment_count, $category_count];
+
+
+                                for ($i = 0; $i < 4; $i++) {
+
+                                    echo "['{$elements[$i]}'" . "," . "{$elements_values[$i]} ],";
+                                }
+
+
+                                ?>
+
+
+                              
+
+                            ]);
+
+                            var options = {
+                                chart: {
+                                    title: 'Admin Record',
+                                    subtitle: '',
+                                }
+                            };
+
+                            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                            chart.draw(data, google.charts.Bar.convertOptions(options));
+                        }
+                    </script> -->
+
+
+                <div id="columnchart_material" style="width:'auto'; height: 500px;"></div>
+            </div>
 
 
             <!-- // 8114390434  20666  HP Services Center Odisha
@@ -425,11 +534,11 @@ include "index_function.php";
 
 
 
-            </div>
-            <!-- /.container-fluid -->
-
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
 
     </div>
     <!-- /#wrapper -->
